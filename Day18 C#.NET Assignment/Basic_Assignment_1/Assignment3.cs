1d3280fc067eb3cdd35d0efce2d09b23b636f96a
Chetan A. Muneshwar <126662915+ChetanMuneshwar@users.noreply.github.com>
2026-03-11 17:29:11 +0530

    Add files via upload

---
+++ Day18 C#.NET Assignment/Basic_Assignment_1/Assignment3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Assignment_1
+{
+    internal class Assignment3
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, n4, n5, sum, avg;
+
+            Console.Write("Enter first number: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter third number: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter fourth number: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter fifth number: ");
+            n5 = Convert.ToDouble(Console.ReadLine());
+
+            sum = n1 + n2 + n3 + n4 + n5;
+            avg = sum / 5;
+            Console.WriteLine();
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Average = " + avg);
+        }
+    }
+}
