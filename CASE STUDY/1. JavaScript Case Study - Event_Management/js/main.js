b83a4f623b7ad33d6643ffd8321a4908123636a6
Chetan A. Muneshwar <126662915+ChetanMuneshwar@users.noreply.github.com>
2026-03-11 13:38:11 +0530

    Delete CASE STUDY directory

--- CASE STUDY/1. JavaScript Case Study - Event_Management/js/main.js
+++
@@ -1,8 +0,0 @@
-import { loadEvents, handleEventForm } from "./controllers/eventController.js"
-import { handleRegistration } from "./controllers/registrationController.js"
-
-document.addEventListener("DOMContentLoaded", () => {
-    loadEvents()
-    handleEventForm()
-    handleRegistration()
-})
